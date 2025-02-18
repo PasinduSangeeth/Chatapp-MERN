d1a1977d977b6bf02f1405c2e57a9b7decc16289
Pasindu <pasindusangeeth968@gmail.com>
2025-02-18 12:59:24 +0530

    message controller creted and brougth users to sidebars

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,10 @@
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredusers = await User.find({ _id: { $ne: loggedInUserId }}).select("-password");
+        res.status(200).json(filteredusers);
+    } catch (error) {
+        console.log("Error in getUsersForSidebar controller:", error.message);
+        res.status(500).json({ message: "Internal server Error" });
+    }
+}
