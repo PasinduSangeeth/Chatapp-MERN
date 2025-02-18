fc05f06aa1c46ffe2cea1db02ced7382dd8844ce
Pasindu <pasindusangeeth968@gmail.com>
2025-02-18 11:31:23 +0530

    profile update part- connected to cloudinary to image upload process

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -41,10 +41,37 @@ export const signup = async (req, res) => {
 };
 
 
-export const login = (req, res) => {
-    res.send('Login route');
-};
+export const login = async (req, res) => {
+    const {email,password}=req.body;
+    try {
+        const user = await User.findOne({email: email});
+        if (!user) {
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        const isPAsswordCorrect= await bcrypt.compare(password, user.password);
+        if (!isPAsswordCorrect) {
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        //generate jwt token here 
+        generateToken(user._id,res)
+        res.status(200).json({
+            _id:user._id,
+            email:user.email,
+            fullName:user.fullName,
+            profilePic:user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller:",error.message);
+        res.status(500).json({message:"Internal server Error"});      
+    }
+};  
 
 export const logout = (req, res) => {
-    res.send('Logout route');
+    try {
+        res.cookie('jwt',"",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller:",error.message);
+        res.status(500).json({message:"Internal server Error"});      
+    }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 // Load environment variables from.env file
 dotenv.config();
@@ -11,6 +12,7 @@ const app = express();
 
 const PORT =process.env.PORT||5002 ;
 app.use(express.json());
+app.use(cookieParser()); // Parse cookies to req.cookies object
 app.use("/api/auth",authRoutes);
 
 
