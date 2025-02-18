6360b9f2c1ba62baec94646175963992e2575072
Pasindu <pasindusangeeth968@gmail.com>
2025-02-18 09:14:11 +0530

    signup route completed

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,32 +1,45 @@
 import User from '../models/user.model.js';
-import bcrypt from 'bcrypt.js';
+import { generateToken } from '../lib/utils.js';
+import bcrypt from 'bcryptjs';
 export const signup = async (req, res) => {
-    const {fullname,email,password}=req.body;
+    const {fullName,email,password}=req.body;
     try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({message:"All fields are required"});}
         //hash password
         if (password.length < 6) {
             return res.status(400).json({message:"Password must be at least 6 characters long"});
         }
         const user= await User.findOne({email});
         if (user) {
-            return res.status(400).json({message:"Email already exists"});
-            const salt= await bcrypt.genSalt(10);
-            const hashedPassword= await bcrypt.hash(password,salt);
-            const newUser=new User({fullname,email,password:hashedPassword});
-        }
+            return res.status(400).json({message:"Email already exists"});}
+            //hash pasword and create a new user
+        const salt= await bcrypt.genSalt(10);
+        const hashedPassword= await bcrypt.hash(password,salt);
+        const newUser=new User({fullName,email,password:hashedPassword});
+        
         if (newUser) {
            //generate jwt token here 
+           generateToken(newUser._id,res)
+           await newUser.save();
+           res.status(201).json({
+            _id:newUser._id,
+            email:newUser.email,
+            fullName:newUser.fullName,
+            profilePic:newUser.profilePic,
+           });
 
-        else{
+}        else{
             return res.status(400).json({message:"Invalid user data"});
         }
 
 } catch (error) {
-        
+        console.log("Error in signup controller:",error.message);
+        res.status(500).json({message:"Internal server Error"});      
 
     };
 };
-};
+
 
 export const login = (req, res) => {
     res.send('Login route');
--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,11 @@ import {connectDB} from './lib/db.js';
 dotenv.config();
 
 const app = express();
-const PORT =process.env.PORT ;
+
+const PORT =process.env.PORT||5002 ;
+app.use(express.json());
 app.use("/api/auth",authRoutes);
 
-app.use.use(express.json());
 
 app.listen(PORT, () => {
     console.log('Server is running on PORT:'+PORT);
