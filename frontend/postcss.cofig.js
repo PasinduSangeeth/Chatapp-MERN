3c21cbc0155656dd4e603eb2d470ba42cc82b425
Pasindu <pasindusangeeth968@gmail.com>
2025-02-19 12:38:29 +0530

    tailwind css plugin-dailsyui installed

---
+++ frontend/postcss.cofig.js
@@ -0,0 +1,7 @@
+// filepath: /i:/chat app/frontend/postcss.config.js
+export default {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// filepath: /i:/chat app/frontend/tailwind.config.js
+import daisyui from 'daisyui'
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [daisyui],
+  }
