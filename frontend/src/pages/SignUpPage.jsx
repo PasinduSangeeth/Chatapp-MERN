b1516b99cc6360276096781908f8f5fd16862205
Pasindu <pasindusangeeth968@gmail.com>
2025-02-24 15:27:52 +0530

    e mail and creaete account button created in signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import { MessageSquare, User ,Mail} from "lucide-react";
+import { MessageSquare, User, Mail, Lock, Eye, EyeOff, Loader } from "lucide-react";
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore.js";
 
@@ -74,12 +74,46 @@ const SignUpPage = () => {
                 />
               </div>
             </div>
-          
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="input input-bordered w-full pl-10"
+                  placeholder="********"
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <EyeOff className="size-5 text-base-content/40" /> : <Eye className="size-5 text-base-content/40" />}
+                </button>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
+              {isSigningUp ? (
+                <>
+                  <Loader className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ):(
+                "Create Account"
+              )}
+            </button>
           </form>
         </div>
       </div>
     </div>
   );
+
 };
 
 export default SignUpPage;
